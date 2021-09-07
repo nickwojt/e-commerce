79830058a8dfe5a25a9a9359c1d2865aeb634f9e
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-09-07 16:52:51 -0600

    Finalized and tested all routes

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# e-commerce
+# E-Commerce Back End
+
+## Description
+
+This project uses mysql2, sequelize, and express to make a functional back-end for an e-commerce site. We use insomia to test all our routes.
+
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+[Description](#description)
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contributing](#contributing)
+
+[Tests](#tests)
+
+[Questions](#questions)
+
+## Installation
+
+Run
+
+    npm i
+
+to pull in dependencies.
+
+## Usage
+
+Run
+
+    npm start
+
+to run the program.
+
+## License
+    
+MIT License 
+    
+https://opensource.org/licenses/MIT
+
+## Contributing
+
+Contact nicholaswojt@outlook.com to a get a pull request.
+
+## Tests
+
+Run
+
+    npm run test
+
+to test the program
+
+## Questions
+
+Contact nicholaswojt@outlook.com for any questions.
+
+Github username: nickwojt
+
+Link to my github profile: https://github.com/nickwojt
+
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,75 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.Create(req.body);
+    res.json("Created");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json("Updated");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json("Deleted");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
