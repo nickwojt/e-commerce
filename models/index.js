268779601dad4c528d20088e35c6c724784effcd
Nicholas Wojtalewicz <nicholaswojt@outlook.com>
2021-09-07 09:40:32 -0600

    Added functionality

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require("./ProductTag");
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
 // Categories have many Products
@@ -17,10 +18,13 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: "Product",
+  through: ProductTag,
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
